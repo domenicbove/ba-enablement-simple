55175879402a8a3cdb0dec14ab1827115c0f4ec5
Domenic Bove <domenicbove@gmail.com>
2015-08-25 09:35:26 -0400

    built maven

---
+++ simpleproject/src/main/java/com/simpleproject/redhat/App.java
@@ -0,0 +1,13 @@
+package com.simpleproject.redhat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
